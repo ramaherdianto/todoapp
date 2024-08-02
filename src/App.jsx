b1fe458ff025e3403d55c6db5f785448750278fd
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-02 21:24:52 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className='text-4xl font-bold text-red-300'>Hello Tailwindcss</h1>
+        </>
+    );
+}
+
+export default App;
