fedae6713c38a4ddf3c0177d106306fae77f7f1f
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-03 16:19:37 +0700

    feat: create add task feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,81 @@ import CheckList from './components/CheckList';
 import Footer from './components/Footer';
 
 function App() {
+    const [tasksList, setTasksList] = useState([]);
+    const [showAlert, setShowAlert] = useState(false);
+    const [formTask, setFormTask] = useState({
+        id: new Date().getTime(),
+        title: '',
+        description: '',
+        completed: false,
+    });
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        onResetForm();
+    };
+
+    const onFormChange = (e) => {
+        const { name, value } = e.target;
+        setFormTask({
+            ...formTask,
+            [name]: value,
+        });
+    };
+
+    const onResetForm = () => {
+        setFormTask({
+            title: '',
+            description: '',
+        });
+    };
+
+    const handleSubmitTask = (event) => {
+        event.preventDefault();
+        if (formTask.title.trim() === '' || formTask.description.trim() === '') {
+            setShowAlert(true);
+            alert('Input field cannot be empty my dear😘');
+        } else {
+            const newTask = {
+                id: new Date().getTime(),
+                title: formTask.title,
+                description: formTask.description,
+                completed: false,
+            };
+            console.log(newTask);
+            setTasksList([...tasksList, newTask]);
+            setOpen(false);
+            onResetForm();
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent default form submission
+            handleSubmitTask(event);
+        }
+    };
+
     return (
         <>
             <main className='min-h-screen mx-auto w-full'>
                 <Header />
-                <Form />
-                <CheckList />
+                <Form
+                    handleKeyDown={handleKeyDown}
+                    onFormChange={onFormChange}
+                    handleSubmitTask={handleSubmitTask}
+                    formTask={formTask}
+                    onResetForm={onResetForm}
+                    open={open}
+                    handleOpen={handleOpen}
+                    handleClose={handleClose}
+                />
+                <CheckList tasksList={tasksList} />
                 <Footer />
             </main>
         </>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Form = () => {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
+const Form = ({
+    handleKeyDown,
+    handleSubmitTask,
+    formTask,
+    onResetForm,
+    onFormChange,
+    open,
+    handleClose,
+    handleOpen,
+}) => {
     return (
         <>
             <section className='px-4 py-10 flex items-center justify-center'>
@@ -35,41 +35,58 @@ const Form = () => {
                     {open ? (
                         <div className='bg-zinc-200 bg-opacity-80 fixed inset-0 z-50 px-4'>
                             <div className='flex justify-center items-center'>
-                                <form className='w-full sm:w-[80%] md:w-[65%] lg:w-1/2 flex relative flex-col justify-center items-start bg-white py-12 px-4 border-[2px] border-b-[6px] border-r-[6px] border-slate-800 rounded-xl gap-8'>
+                                <div className='w-full sm:w-[80%] md:w-[65%] lg:w-[40%] flex relative flex-col justify-center items-start bg-white py-12 px-4 border-[2px] border-b-[6px] border-r-[6px] border-slate-800 rounded-xl'>
                                     <div className='flex w-full justify-center items-center'>
                                         <h1 className='font-bold text-xl'>Add Task</h1>
                                         <button
                                             onClick={handleClose}
-                                            className='absolute w-0 h-0 -top-[13.5em] sm:-top-[14.5em] right-[2em]'
+                                            className='absolute w-0 h-0 -top-[12em] sm:-top-[12em] right-[2em] sm:right-[2em]'
                                         >
                                             ❌
                                         </button>
                                     </div>
-                                    <div className='w-full flex flex-col items-center sm:items-start gap-5 text-lg  text-zinc-600 mt-10'>
-                                        <input
-                                            type='text'
-                                            name=''
-                                            placeholder='Title'
-                                            id=''
-                                            className='bg-gray-50 font-medium w-full sm:w-1/2 md:w-[350px] border border-b-[6px] border-r-[6px] border-slate-800 focus:outline-none text-slate-800 text-sm block px-2.5 py-4 rounded-lg'
-                                        />
-                                        <textarea
-                                            id='message'
-                                            rows='4'
-                                            className='block font-medium p-2.5 lg:h-[100px] w-full text-sm bg-gray-50 border border-b-[6px] border-r-[6px] border-slate-800 focus:outline-none text-slate-800 rounded-lg'
-                                            placeholder='Description...'
-                                        />
-                                    </div>
-
-                                    <div className='flex justify-center sm:mt-10'>
-                                        <button
-                                            type='submit'
-                                            className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-slate-800 font-medium text-sm px-6 py-5 rounded-lg'
+                                    <div className='w-full flex justify-center items-center text-lg text-zinc-600 mt-14'>
+                                        <form
+                                            onSubmit={handleSubmitTask}
+                                            className='w-full flex items-start flex-col rounded-xl'
                                         >
-                                            Add
-                                        </button>
+                                            <input
+                                                type='text'
+                                                name='title'
+                                                value={formTask.title}
+                                                onChange={onFormChange}
+                                                onKeyDown={handleKeyDown}
+                                                placeholder='Title'
+                                                id=''
+                                                className='bg-gray-50 font-medium w-1/2 sm:w-1/2 md:w-[350px] border border-b-[6px] border-r-[6px] border-slate-800 focus:outline-none text-slate-800 text-sm block px-2.5 py-4 rounded-lg'
+                                            />
+                                            <textarea
+                                                id='message'
+                                                rows='4'
+                                                name='description'
+                                                value={formTask.description}
+                                                onChange={onFormChange}
+                                                className='block font-medium my-4 p-2.5 lg:h-[100px] w-full text-sm bg-gray-50 border border-b-[6px] border-r-[6px] border-slate-800 focus:outline-none text-slate-800 rounded-lg'
+                                                placeholder='Description...'
+                                            />
+                                            <div className='flex items-center justify-center mt-4 gap-4'>
+                                                <button
+                                                    type='submit'
+                                                    className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-slate-800 font-medium text-sm px-6 py-3 rounded-lg'
+                                                >
+                                                    Add
+                                                </button>
+                                                <button
+                                                    onClick={onResetForm}
+                                                    type='reset'
+                                                    className='text-slate-800 focus:outline-none  font-medium text-sm px-6 py-3 rounded-lg'
+                                                >
+                                                    Reset
+                                                </button>
+                                            </div>
+                                        </form>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     ) : null}
